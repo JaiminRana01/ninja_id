35c5296a56df8e0978436ad1e3d6066cfed2ed1b
JaiminRana01 <jaiminr625@gmail.com>
2021-05-09 21:07:25 +0530

    added stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,14 @@ void main() {
   ));
 }
 
-class JaiminCard extends StatelessWidget {
+class JaiminCard extends StatefulWidget {
+  @override
+  _JaiminCardState createState() => _JaiminCardState();
+}
+
+class _JaiminCardState extends State<JaiminCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +23,15 @@ class JaiminCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -65,7 +81,7 @@ class JaiminCard extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
